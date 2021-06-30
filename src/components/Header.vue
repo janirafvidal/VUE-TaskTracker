c1df44c9dc9ac342e10214c54fd16ee37ded7b9c
Janira F. Vidal <78372643+janirafvidal@users.noreply.github.com>
2021-06-30 18:30:41 +0200

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <header>
+    <h1>{{ $props.title }}</h1>
+    <Button
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
+
+    <!-- <Button text="Update Task" color="blue" />
+    <Button text="Delete Task" color="red" /> -->
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+
+export default {
+  name: "Header",
+  props: {
+    title: String,
+    showAddTask: Boolean,
+  },
+  components: {
+    Button,
+  },
+  created() {
+    console.log("test" + this.$props.title);
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
